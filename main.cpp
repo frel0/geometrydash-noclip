f8021d5e33e37a88251d501de8fc62397456db6f
frel0 <90924169+frel0@users.noreply.github.com>
2023-02-04 10:51:20 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <Windows.h>
+#include <iostream>
+
+int main()
+{
+    DWORD processId;
+
+    HWND hwnd = FindWindow(NULL, "Geometry Dash");
+    GetWindowThreadProcessId(hwnd, &processId);
+    HANDLE handle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processId);
+
+    int val = 14315;
+
+    while (true) {
+    WriteProcessMemory(handle, (LPVOID)0x00EAA1DD, &val, 4, NULL);
+    }
+
+    return 0;
+}
